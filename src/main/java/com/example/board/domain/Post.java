13d74528016c4de74141f7c7c4ad6332acec6f6b
JINSOO <JINSOO@DESKTOP-D8BOKQU>
2025-11-28 19:38:32 +0900

    Post Entity 생성

---
+++ src/main/java/com/example/board/domain/Post.java
@@ -0,0 +1,39 @@
+@Entity
+@Table(name="posts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post{
+    @Id
+    @GeneratedValue(Strategy = GenerationType.IDEN)
+    private Integer Id;
+
+    @Column(nullable=false,length = 200)
+    private String title;
+
+    @Column(nullable=false,columDefinition="TEXT") //VARCHARX 긴글용 TEXT
+    private String content;
+
+    @Column(nullable=false)
+    private Integer viewCount;
+
+    @Column(nullable=false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy="post")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.viewCount = 0;
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
