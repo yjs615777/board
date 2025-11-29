742e7d120ce7b2d00e7ab88e01b6ee9b8214ed7d
JINSOO <JINSOO@DESKTOP-D8BOKQU>
2025-11-29 15:48:51 +0900

    -UserRepository 코드 수정 -PostRepositroy 생성 -CommentRepository 생성

--- src/main/java/com/example/board/repository/UserRepository.java
+++ src/main/java/com/example/board/repository/UserRepository.java
@@ -4,11 +4,8 @@ import com.example.board.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // username으로 유저 찾기
+    // 로그인용: username으로 유저 찾기
     Optional<User> findByUsername(String username);
-
-    // email로 유저 찾기
-    Optional<User> findByEmail(String email);
 
     // username 중복 체크
     boolean existsByUsername(String username);
